b0295225f79b8541644a3f95a6be4129ffe1254a
Thomas Zaffran <thomas.zaffran@smartjog.com>
2007-05-25 12:35:32 +0000

    indexparse.c can read indexer output. Output format is  : Magic Number : 571DE20501D0ABCD (64 bits) Version (16 bits)
    For each frame :
    PTS (64 bits)
    DTS (64 bits)
    PES Offset (64 bits)
    timecode frames number (32 bits)
    timecode seconds number (32 bits)
    timecode minutes number (32 bits)
    timecode hours number (32 bits)

--- indexer.c
+++ indexer.c
@@ -31,6 +31,7 @@ typedef struct {
     int32_t seconds;
     int32_t frames;
 } Timecode;
+
 typedef struct {
     uint8_t pic_type;
     int seq:1;
@@ -74,8 +75,6 @@ static int write_eof_marker(StreamContext *s)
     return 0;
 }
 
-
-
 extern AVInputFormat mpegps_demuxer;
 
 extern const uint8_t *ff_find_start_code(const uint8_t *p, const uint8_t *end, uint32_t *state);
@@ -100,6 +99,7 @@ static av_always_inline offset_t pes_find_packet_start(ByteIOContext *pb, offset
     }
     abort();
 }
+
 static int write_index(StreamContext *stcontext)
 {
     ByteIOContext indexpb;
@@ -110,26 +110,25 @@ static int write_index(StreamContext *stcontext)
     int i;
 
     qsort(stcontext->index, stcontext->frame_num, sizeof(Index), ind_sort_by_pts);
-    put_le64(&indexpb, 0x766978004BABDAEDLL);
+    put_le64(&indexpb, 0x571DE20501D0ABCDLL);           // Magic number
+    put_le16(&indexpb, 0x0000);                         // Version
     for (i = 0; i < stcontext->frame_num; i++) {
         Index *ind = &stcontext->index[i];
-        put_le64(&indexpb, ind->pts);
-        put_le64(&indexpb, ind->dts);
-        put_le64(&indexpb, ind->pes_offset);
-        put_le32(&indexpb, ind->timecode.frames);
-        put_le32(&indexpb, ind->timecode.seconds);
-        put_le32(&indexpb, ind->timecode.minutes);
-        put_le32(&indexpb, ind->timecode.hours);
-//        printf("ind->pts %lld, ind->dts %lld, pes_offset %lld\ntimecode : %02d:%02d:%02d:%02d\n", ind->pts, ind->dts, ind->pes_offset, ind->timecode.hours, ind->timecode.minutes, ind->timecode.seconds, ind->timecode.frames);
+        put_le64(&indexpb, ind->pts);                   // Frame pts
+        put_le64(&indexpb, ind->dts);                   // Frame dts
+        put_le64(&indexpb, ind->pes_offset);            // PES Offset
+        put_le32(&indexpb, ind->timecode.frames);       // Timecode frame number
+        put_le32(&indexpb, ind->timecode.seconds);      // Timecode seconds number
+        put_le32(&indexpb, ind->timecode.minutes);      // Timecode minutes number
+        put_le32(&indexpb, ind->timecode.hours);        // Timecode hours number
         put_flush_packet(&indexpb);
     }
     index_size = url_close_dyn_buf(&indexpb, &index_buf);
     put_flush_packet(&indexpb);
     printf("index size %d\n", index_size);
-    //url_fopen(&indexpb, "index", URL_WRONLY);
+
     put_buffer(&stcontext->opb, index_buf, index_size);
     put_flush_packet(&stcontext->opb);
-    //url_fclose(&stcontext->opb);
     return 0;
 }
 static int write_trailer(StreamContext *s)
@@ -144,10 +143,12 @@ static av_always_inline int ind_set(StreamContext *stc, Index *ind, AVPacket *pk
 {
     Index *oldind = stc->frame_num ? &stc->index[stc->frame_num - 1] : NULL;
     offset_t pkt_start = url_ftell(&stc->fc->pb) - pkt->size;
+    // Record PES Offset
     ind->pes_offset = pes_find_packet_start(&stc->fc->pb, pkt_start, st->id);
+    ind->pes_offset = pkt->pos;
+    // Record DTS and PTS
     ind->dts = stc->current_dts[st->index];
     ind->pts = stc->current_pts[st->index];
-    ind->pes_offset = pkt->pos;
     if (oldind && ind->dts <= oldind->dts) {
         ind->dts = oldind->dts + 3600;
         ind->pts = oldind->pts + 3600;
@@ -155,7 +156,6 @@ static av_always_inline int ind_set(StreamContext *stc, Index *ind, AVPacket *pk
     }
     return 0;
 }
-#define BUFFER_SIZE 262144
 
 static int get_frame_rate(AVStream *st, AVPacket *pkt)
 {
@@ -163,7 +163,7 @@ static int get_frame_rate(AVStream *st, AVPacket *pkt)
         if (st->codec->codec_type == CODEC_TYPE_VIDEO) {
             uint8_t *buf = pkt->data;
             float fps = -1;
-            //printf("buf 7 : %x\n", buf[7]);
+
             switch (buf[7] & 0xF){
                 case 0x1:
                     fps = 24000/1001;
@@ -202,33 +202,29 @@ static int get_frame_rate(AVStream *st, AVPacket *pkt)
 
 static int find_timecode(Index *ind, AVStream *st, AVPacket *pkt, int *kf, Timecode *last_key_frame, float fps)
 {
-//    int drop = 0,  hours = 0, minutes = 0, seconds = 0, frames = 0;
+    int drop = 0;
     if (st) {
         if (st->codec->codec_type == CODEC_TYPE_VIDEO) {
             uint8_t *buf = pkt->data;
             uint32_t c = -1, d = -1;
             int i, j;
-            for(i = 0; i < pkt->size - 4 && d != 0x100; i++) {
-                if (c != 0x1B8){
+            for(i = 0; i < pkt->size - 4 && d != PICTURE_START_CODE; i++) {
+                if (c != GOP_START_CODE){
                     c = (c<<8) + buf[i]; 
                     j = i+1;
                 }
                 d = (d<<8) + buf[i];
-//                printf("%d buf[i]: %x\t c : %x\t d : %x\n",i, buf[i], c, d);
             } 
-//            printf ("c : %x\n", c);
-            if (c == 0x1B8) { // found GOP header => I-frame follows 
-//                drop = !!(buf[i] & 0x80);
+            if (c == GOP_START_CODE) { // found GOP header :  
+                drop = !!(buf[j] & 0x80);
                 last_key_frame->hours   = ind->timecode.hours   = (buf[j] >> 2) & 0x1f;
                 last_key_frame->minutes = ind->timecode.minutes = (buf[j] & 0x03) << 4 | (buf[j+1] >> 4);
                 last_key_frame->seconds = ind->timecode.seconds = (buf[j+1] & 0x07) << 3 | (buf[j+2] >> 5);
                 last_key_frame->frames  = ind->timecode.frames  = (buf[j+2] & 0x1f) << 1 | (buf[j+3] >> 7);
-                printf("GOP timecode :\t%02d:%02d:%02d:%02d\n", last_key_frame->hours, last_key_frame->minutes, last_key_frame->seconds, last_key_frame->frames);
                 (*kf)++;
             }
-            if (d == 0x100) { // found picture start code
-                printf("Pic Start Code\n");
-                uint32_t temp_ref=0;
+            if (d == PICTURE_START_CODE) { // found picture start code
+                uint32_t temp_ref = 0;
                 uint32_t frame_type;
                 int round_fps = (int)fps;
                 temp_ref = (temp_ref<<8) + buf[i];
@@ -236,8 +232,9 @@ static int find_timecode(Index *ind, AVStream *st, AVPacket *pkt, int *kf, Timec
                 temp_ref = (temp_ref>>6);
                 frame_type = buf[i+1];
                 frame_type = (frame_type >> 3) & 0x7;
-                printf("frame type : %x, temp_ref : %d\n", frame_type, temp_ref);
-                // calculation of timecode for current frame
+                printf("\nframe type : %x, temp_ref : %d\n", frame_type, temp_ref);
+
+                // calculation of timecode for the current frame using the last recorded GOP timecode and the temporal reference
                 ind->timecode.frames  = (last_key_frame->frames  + temp_ref) % round_fps;
                 ind->timecode.seconds = ((int)(last_key_frame->seconds + ((last_key_frame->frames + temp_ref) / fps))) % 60;
                 ind->timecode.minutes = ((int)(last_key_frame->minutes + ((last_key_frame->frames + temp_ref) / (fps * 60)))) % 60;
@@ -248,12 +245,14 @@ static int find_timecode(Index *ind, AVStream *st, AVPacket *pkt, int *kf, Timec
                     }
                     ind->timecode.minutes = (ind->timecode.minutes + 1) % 60;
                 }
-            printf("timecode :\t%02d:%02d:%02d:%02d\n", ind->timecode.hours, ind->timecode.minutes, ind->timecode.seconds, ind->timecode.frames);
+                printf("PIC timecode :\t%02d:%02d:%02d:%02d\n", ind->timecode.hours, ind->timecode.minutes, ind->timecode.seconds, ind->timecode.frames);
             } 
         }
     }
     return 0;
 }
+
+#define BUFFER_SIZE 262144
 
 int main(int argc, char *argv[])
 {
---
+++ indexparse.c
@@ -0,0 +1,46 @@
+#include <ffmpeg/avformat.h>
+
+int main(int argc, char **argv)
+{
+    ByteIOContext pb1, *pb = &pb1;
+    ByteIOContext mpeg1, *mpeg = NULL;
+//    uint8_t buffer[16];
+//    offset_t header_offset;
+//    offset_t first_offset;
+//    uint64_t magic;
+
+    if (argc < 2) {
+        printf("usage: indexparse <index file>\n");
+        return 1;
+    }
+
+    register_protocol(&file_protocol);
+    if (argc == 3) {
+        if (url_fopen(&mpeg1, argv[2], URL_RDONLY) < 0) {
+            printf("error opening file %s\n", argv[2]);
+            return 1;
+        }
+        mpeg = &mpeg1;
+    }
+
+    if (url_fopen(pb, argv[1], URL_RDONLY) < 0) {
+        printf("error opening file %s\n", argv[1]);
+        return 1;
+    }
+
+
+/*#define DUMP(offset) \
+    url_fseek(mpeg, offset, SEEK_SET); \
+    get_buffer(mpeg, buffer, 16); \
+    av_hex_dump(stdout, buffer, 16); \*/
+    printf("magic %llx\n", get_le64(pb));
+    printf("Version : %d\n", get_le16(pb));
+    while (!url_feof(pb)) {
+        printf("dts %lld\n", get_le64(pb));
+        printf("pts %lld\n", get_le64(pb));
+        printf("pes_offset %lld\n", get_le64(pb));
+        printf("Timecode : %02d:%02d:%02d:%02d\n", get_le32(pb), get_le32(pb), get_le32(pb), get_le32(pb));
+    }
+    url_fclose(pb);
+    return 0;
+}
