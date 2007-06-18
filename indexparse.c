d1ddeb5ab29e8a1d12412f8ad1f78387f70fa756
Thomas Zaffran <thomas.zaffran@smartjog.com>
2007-06-18 14:35:24 +0000

    undo previous changes, framerate won't be needed after all

--- indexparse.c
+++ indexparse.c
@@ -35,7 +35,6 @@ int main(int argc, char **argv)
     av_hex_dump(stdout, buffer, 16); \*/
     printf("magic %llx\n", get_le64(pb));
     printf("Version : %d\n", get_byte(pb));
-    printf("FPS : %d\n", get_byte(pb));
     while (!url_feof(pb)) {
         printf("-----------------------\n");
         printf("pts %lld\n", get_le64(pb));
