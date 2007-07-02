629449932fc7a4e4c4e011871c35cf3571e06816
Thomas Zaffran <thomas.zaffran@smartjog.com>
2007-07-02 17:00:55 +0000

    added a function to look for the closest previous I-frame (will be needed for seeking) -> added a field in search context to store the offset where the given frame was found and search back to the closest I frame. search_frame() now takes the search context's adress as argument instead of its value

--- search_idx.c
+++ search_idx.c
@@ -10,6 +10,7 @@ typedef struct{
     uint64_t size;
     ByteIOContext *pb;
     uint32_t search_time;
+    int index_binary_offset;
 } SearchContext;
 
 static av_always_inline int compute_idx(Index *read_idx, ByteIOContext *seek_pb)
@@ -25,37 +26,51 @@ static av_always_inline int compute_idx(Index *read_idx, ByteIOContext *seek_pb)
     return read_idx->timecode.hours * 1000000 + read_idx->timecode.minutes * 10000 + read_idx->timecode.seconds * 100 + read_idx->timecode.frames;
 }
 
-int search_frame(SearchContext search, Index *read_idx)
+int search_frame(SearchContext *search, Index *read_idx)
 {
     int low = 0;
-    int mid = search.size / 2;
+    int mid = search->size / 2;
     ByteIOContext *seek_pb = NULL;
     uint32_t read_time; // used to store the timecode members in a single 32 bits integer to facilitate comparison 
-    int nb_index = (int)(search.size / INDEX_SIZE);
+    int nb_index = (int)(search->size / INDEX_SIZE);
 
     printf("%d indexes\n", nb_index);
-    seek_pb = search.pb;
+    seek_pb = search->pb;
 
     // Checks if the timecode we want is inferior to the first timecode in the file
     read_time = compute_idx(read_idx, seek_pb);
-    if (read_time == search.search_time){
+    if (read_time == search->search_time){
         return 1;
-    } else if (read_time > search.search_time) {
+    } else if (read_time > search->search_time) {
         return -1;
     }
-    while (low <= search.size) {
-        mid = (int)((search.size + low) / 2);
+    while (low <= search->size) {
+        mid = (int)((search->size + low) / 2);
         mid -= (mid % INDEX_SIZE) - HEADER_SIZE ;
 
         url_fseek(seek_pb, mid, SEEK_SET);
         read_time = compute_idx(read_idx, seek_pb);
-        if (read_time == search.search_time){
+        if (read_time == search->search_time){
+            search->index_binary_offset = mid;
             return 1;
-        } else if (read_time > search.search_time) {
-            search.size = mid - INDEX_SIZE;
-        } else if (read_time < search.search_time) {
+        } else if (read_time > search->search_time) {
+            search->size = mid - INDEX_SIZE;
+        } else if (read_time < search->search_time) {
             low = mid + INDEX_SIZE;
         }
+    }
+    return 0;
+}
+
+int find_previous_I_frame(Index *I_frame, Index read_idx, SearchContext search)
+{
+    int i = search.index_binary_offset - INDEX_SIZE;
+    ByteIOContext *seek_pb = search.pb;
+
+    while (i >= 0 && I_frame->pic_type != 1){
+        url_fseek(seek_pb, i, SEEK_SET);
+        compute_idx(I_frame, seek_pb);
+        i -= INDEX_SIZE;
     }
     return 0;
 }
@@ -65,7 +80,7 @@ int main(int argc, char **argv)
     SearchContext search;
     ByteIOContext pb1;
     ByteIOContext mpeg1, *mpeg = NULL;
-    Index read_idx;
+    Index read_idx, I_frame;
 
     search.pb = &pb1;
     if (argc < 3) {
@@ -100,7 +115,7 @@ int main(int argc, char **argv)
 //    printf("Version : %d\n", get_byte(search.pb));
     printf("Looking for frame with timecode : %c%c:%c%c:%c%c:%c%c\n",argv[2][0], argv[2][1], argv[2][2], argv[2][3], argv[2][4], argv[2][5], argv[2][6], argv[2][7]);
 
-    int res = search_frame(search, &read_idx); 
+    int res = search_frame(&search, &read_idx); 
     if (!res){
         printf("Frame could not be found, check input data\n");
     } else if (res == -1) {
@@ -120,7 +135,12 @@ int main(int argc, char **argv)
             default :
                 printf("type of frame unknown\n");
         }
-        printf("DTS : %lld\nPTS : %lld\nType of frame : %c\n", read_idx.dts, read_idx.pts, frame);
+        if (frame != 'I'){
+            find_previous_I_frame(&I_frame, read_idx, search);
+            printf("------- Closest previous I-Frame : -------\n");
+            printf("Offset : %lld\n", I_frame.pes_offset);
+        }
+        printf("\n------Frame-------\nDTS : %lld\nPTS : %lld\nType of frame : %c\n", read_idx.dts, read_idx.pts, frame);
         printf("Offset : %lld\n", read_idx.pes_offset);
     }
     url_fclose(search.pb);
