e0885e05c4d7cd83fa2bd2a200e5d7b0648944ec
Thomas Zaffran <thomas.zaffran@smartjog.com>
2007-07-06 12:56:32 +0000

    changed INDEX_SEARCH for SJ_SEARCH_H, define version number

--- libsjindex/sj_search_index.h
+++ libsjindex/sj_search_index.h
@@ -1,5 +1,7 @@
-#ifndef INDEX_SEARCH
-#define INDEX_SEARCH
+#ifndef SJ_SEARCH_H
+#define SJ_SEARCH_H
+
+#define LIBSJINDEX_VERSION 0.0.1
 
 #define SJ_INDEX_TIMECODE_SEARCH 1
 #define SJ_INDEX_PTS_SEARCH 2
@@ -18,5 +20,5 @@ int sj_index_load(char *filename, SJ_IndexContext *sj_ic);
 int sj_index_unload(SJ_IndexContext *sj_ic);
 char sj_index_get_frame_type(Index idx);
 int sj_index_search(SJ_IndexContext *sj_ic, uint64_t search_val, Index *idx, Index *key_frame, uint64_t flags);
-#endif 
+#endif /* SJ_SEARCH_H */ 
 
