7d3665563ca94ac53238d6172211770caf39dd47
Thomas Zaffran <thomas.zaffran@smartjog.com>
2007-07-06 09:02:38 +0000

    create sjindex directory in /usr/local/lib/ for the library

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-#Shared sj_index librarie installation
+#Shared sjindex library installation
 
 # compiling the library
 cd libsjindex && make
 
 # copy library in /usr/local/ and headers in /usr/local/include
 cp *.h /usr/local/include
-cp libsjindex.so.* /usr/local/
+mkdir /usr/local/lib/sjindex
+cp libsjindex.so.* /usr/local/lib/sjindex/
 
 # fixing up symbolic link and set up soname
-/sbin/ldconfig -n /usr/local/ 
+/sbin/ldconfig -n /usr/local/lib/sjindex/
 
 # setting up linker name
-ln -sf /usr/local/libsjindex.so.0 /usr/local/libsjindex.so
+ln -sf /usr/local/lib/sjindex/libsjindex.so.0 /usr/local/lib/sjindex/libsjindex.so
 
 cd .. && make
